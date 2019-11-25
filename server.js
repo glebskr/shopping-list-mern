7cace0df61e07cff665b456d960f9dc4aa34c536
glebskr <glesbkr@gmail.com>
2019-11-25 23:46:59 +0300

    make it better

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const db = require('./config/keys').mongoURI;
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static('client/build'))
 
-  app.get('*', (req, res) =>{
+  app.get('/', (req, res) =>{
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
