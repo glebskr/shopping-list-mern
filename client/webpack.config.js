abbae11ae2ef9f366b0568baec8db8427dcd4d11
glebskr <glesbkr@gmail.com>
2019-11-25 22:43:51 +0300

    make it better

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,7 +8,7 @@ const context = path.resolve(__dirname)
 module.exports = {
    
     mode: "production",
-
+    entry: '.tmp/',
     output: {
         path: path.join(__dirname, '/dist'),
         filename: "index-bundle.js"
