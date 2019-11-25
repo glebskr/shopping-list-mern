443bf22ba87ab74e3be800b87517074d403f9ac4
glebskr <glesbkr@gmail.com>
2019-11-25 23:34:27 +0300

    make it better

---
+++ client/build/precache-manifest.5352b9596627fca09afd6abbb0e21ead.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e432bcb9c1d7b248e1ea8bcaf227ffe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07204cfc034d179dfc7e",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "dbf4dc6ddd636d38b7be",
+    "url": "/static/css/main.b5aea271.chunk.css"
+  },
+  {
+    "revision": "07204cfc034d179dfc7e",
+    "url": "/static/js/2.96d33f64.chunk.js"
+  },
+  {
+    "revision": "dbf4dc6ddd636d38b7be",
+    "url": "/static/js/main.ba3750d7.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
